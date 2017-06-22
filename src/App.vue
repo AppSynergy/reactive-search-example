48f3b67afe076c89e2beb817c6f6fa3fb8e7af17
Adam Marshall <adam@appsynergy.net>
2017-06-22 21:50:56 +0100

    add country, basic tiles

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,43 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1></h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <div class="container">
+      <div class="search-ui row">
+        <div class="col-6">
+          Filter by price interval: <b>€ 10</b> <input id="ex2" type="text" class="span2" value="" data-slider-min="10" data-slider-max="1000" data-slider-step="5" data-slider-value="[250,450]"/> <b>€ 1000</b>
+        </div>
+      </div>
+      <div class="search-results row">
+        <div v-for="villa in villas" class="col-12 col-sm-6 col-md-4 col-lg-3">
+          <div class="card my-2 p-3">
+            <h2>{{ villa.title }}</h2>
+            <span v-if="villa.pool">Has a pool.<br></span>
+            <span v-if="villa.near_sea">Near the sea!<br></span>
+            <span><strong>{{ villa.price_per_week }}</strong> per week.</span>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  }
-}
+<script lang="coffee">
+import Villas from './assets/example_data.json'
+
+App =
+  name: 'app'
+  data: () ->
+    msg: 'Welcome to You'
+  computed:
+    villas: () -> Villas
+
+export default App
 </script>
 
 <style lang="sass">
 @import '../node_modules/bootstrap/scss/bootstrap.scss'
 @import url('https://fonts.googleapis.com/css?family=Montserrat|Open+Sans|Material+Icons')
+
+.card
+  min-height: 12em
 
 </style>
