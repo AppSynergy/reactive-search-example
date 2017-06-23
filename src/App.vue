06967e9b098ff2bd9975ec4099756f04e1fe6be0
Adam Marshall <adam@appsynergy.net>
2017-06-23 11:52:48 +0100

    coloring in, filtering

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,49 @@
 <template>
   <div id="app">
     <div class="container">
-      <div class="search-ui row">
-        <div class="col-6">
-          Filter by price interval: <b>€ 10</b> <input id="ex2" type="text" class="span2" value="" data-slider-min="10" data-slider-max="1000" data-slider-step="5" data-slider-value="[250,450]"/> <b>€ 1000</b>
+
+      <div class="search-ui card">
+        <div class="form-inline">
+
+          <div class="form-group">
+            <input type="text" class="form-control"
+              v-model="text_search" value="" placeholder="Filter by title or description">
+          </div>
+
+          <div class="form-group">
+            <label class="form-check-label ml-5">
+              <input class="form-check-input"
+                type="checkbox" value="" v-model="pool">
+              Has a pool.
+            </label>
+            <label class="form-check-label ml-5">
+              <input class="form-check-input"
+                type="checkbox" value="" v-model="near_sea">
+              Near the sea.
+            </label>
+          </div>
+
         </div>
       </div>
+
       <div class="search-results row">
-        <div v-for="villa in villas" class="col-12 col-sm-6 col-md-4 col-lg-3">
-          <div class="card my-2 p-3">
-            <h2>{{ villa.title }}</h2>
-            <span v-if="villa.pool">Has a pool.<br></span>
-            <span v-if="villa.near_sea">Near the sea!<br></span>
-            <span><strong>{{ villa.price_per_week }}</strong> per week.</span>
+        <div v-for="villa in villas" class="col col-12 col-sm-6 col-md-4 col-xl-3">
+          <div class="card my-2">
+            <div class="card-block">
+              <h4 class="card-title">{{ villa.title }}</h4>
+              <span class="badge badge-info my-1"
+                v-if="villa.pool">Has a pool.<br></span>
+              <span class="badge badge-primary my-1"
+                v-if="villa.near_sea">Near the sea.<br></span>
+              <p>{{ villa.description.substr(0,44)+"..." }}</p>
+            </div>
+            <div class="card-footer">
+              <strong>{{ villa.price_per_week }}</strong> per week.
+            </div>
           </div>
         </div>
       </div>
+
     </div>
   </div>
 </template>
@@ -26,9 +54,19 @@ import Villas from './assets/example_data.json'
 App =
   name: 'app'
   data: () ->
-    msg: 'Welcome to You'
+    pool: true
+    near_sea: false
+    text_search: ''
   computed:
-    villas: () -> Villas
+    villas: () ->
+      Villas.filter (x) =>
+        if @pool && !x.pool then return false
+        if @near_sea && !x.near_sea then return false
+        if @text_search.length > 1
+          inDesc = x.description.indexOf(@text_search) != -1
+          inTitle = x.title.indexOf(@text_search) != -1
+          if !inDesc && !inTitle then return false
+        true
 
 export default App
 </script>
@@ -37,7 +75,20 @@ export default App
 @import '../node_modules/bootstrap/scss/bootstrap.scss'
 @import url('https://fonts.googleapis.com/css?family=Montserrat|Open+Sans|Material+Icons')
 
-.card
-  min-height: 12em
+
+body
+  background-color: #f2f2f6
+
+.search-ui
+  margin-top: 40px
+  padding: 20px 15px
+
+.search-results
+  background-color: white
+  padding: 10px 5px
+  margin: 40px 0 80px
+  .card
+    min-height: 15em
+
 
 </style>
